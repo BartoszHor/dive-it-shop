61b34b5583ff833f9a4bf99d1d81c761aabe92cb
Bartosz Horoba <bartosz.horoba@gmail.com>
2021-03-10 15:10:39 +0100

    Initial commit with base structure

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './styles/global.scss';
+
+const App = () => {
+  return (
+    <div>
+      apps
+    </div>
+  );
+};
+
+export default App;
---
+++ client/src/redux/store.js
@@ -0,0 +1,29 @@
+import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
+import thunk from 'redux-thunk';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import {initialState} from './initialState'
+
+
+// import reducers
+
+// combine reducers
+const reducers = {
+  
+};
+
+Object.keys(initialState).forEach(item => {
+  if (typeof reducers[item] == 'undefined') {
+    reducers[item] = (statePart = null) => statePart;
+  }
+});
+
+const combinedReducers = combineReducers(reducers);
+
+// create store
+export const store = createStore(
+  combinedReducers,
+  initialState,
+  composeWithDevTools(
+    applyMiddleware(thunk)
+  )
+);
