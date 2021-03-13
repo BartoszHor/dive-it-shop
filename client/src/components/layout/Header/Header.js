27b77f0e97440d706da779a4475d7244b17a6f6c
Bartosz Horoba <bartosz.horoba@gmail.com>
2021-03-14 00:43:10 +0100

    Add some styles to Header and fix Carousel

--- client/src/components/layout/Header/Header.js
+++ client/src/components/layout/Header/Header.js
@@ -9,17 +9,18 @@ import clsx from 'clsx'
 
 const Header = () => {
   return (    
-    <Navbar bg="light" expand="lg" className={`navbar-expand-sm navi`}>
-    <Navbar.Brand as={Link} to="/">React-Bootstrap</Navbar.Brand>
+    <Navbar bg="info" expand="lg" className={`navbar-expand-sm`}>
+    <Navbar.Brand as={Link} to="/">LOGO</Navbar.Brand>
     <Navbar.Toggle aria-controls="basic-navbar-nav" />
     <Navbar.Collapse id="basic-navbar-nav" className={styles.menuCollapse}>
         <Nav className="ml-auto">
         <Nav.Link as={Link} id={styles.item} to="/">Home</Nav.Link>
-        <NavDropdown title="Offer" className={styles.menuCollapse} id="basic-nav-dropdown">
-            <NavDropdown.Item as={Link} id={styles.item} to="equipment">Equipment</NavDropdown.Item>
-            <NavDropdown.Item as={Link} id={styles.item} to="trips">Trips</NavDropdown.Item>
-            <NavDropdown.Item as={Link} id={styles.item} to="gallery">Gallery</NavDropdown.Item>
-            <NavDropdown.Item as={Link} id={styles.item} to="contact">Contact</NavDropdown.Item>
+        <NavDropdown className={styles.menuCollapse} id="basic-nav-dropdown"
+        title={<span className={styles.offer}>Offer</span>}>
+            <NavDropdown.Item as={Link} id={styles.dropdownItem} to="equipment">Equipment</NavDropdown.Item>
+            <NavDropdown.Item as={Link} id={styles.dropdownItem} to="trips">Trips</NavDropdown.Item>
+            <NavDropdown.Item as={Link} id={styles.dropdownItem} to="gallery">Gallery</NavDropdown.Item>
+            <NavDropdown.Item as={Link} id={styles.dropdownItem} to="contact">Contact</NavDropdown.Item>
         </NavDropdown>
         <Nav.Link as={Link} id={styles.item} to="/login">Login</Nav.Link>
         <Nav.Link as={Link} id={styles.item} to="/not">Cart</Nav.Link>
